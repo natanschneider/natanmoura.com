c2c860cd1b0f6f2b411c4c4cc2a37df56cc79312
natanschneider <natanschneider@protonmail.com>
2025-01-26 11:45:53 -0300

    *feat* add content to index page

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -1,5 +1,20 @@
-export default function index(){
+import { Layout } from '../layout.jsx'
+import { Card, CardTitle, CardLgLink } from '../components/Card.jsx'
+
+export default function index({ projects }){
+    let i = 0;
     return(
-        <div></div>
+        <Layout>
+            { projects.map(item => (
+                <Card key={ i++ }>
+                    <a href={ item.link }>
+                        <CardTitle>{ item.title }</CardTitle>
+                    </a>
+                    <a href={ item.repository }>
+                        <CardLgLink>Portfolio</CardLgLink>
+                    </a>
+                </Card>
+            )) }
+        </Layout>
     )
 }
