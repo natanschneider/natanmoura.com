6f58485798ca35e206104f69b5be27a48632f844
natanschneider <natanschneider@protonmail.com>
2025-01-26 11:46:12 -0300

    *feat* pass correct parameters to index page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('index');
+    return Inertia::render('index', ['projects' => ProjectController::MockedData()]);
 });
