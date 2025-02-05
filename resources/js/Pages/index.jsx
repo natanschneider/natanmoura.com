a5a6addd0d4d0c268e0bf8cf539bf09fc3ade6fd
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 16:30:58 -0300

    *feat* add container div to index page

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -1,20 +1,21 @@
 import { Layout } from '../layout.jsx'
-import { Card, CardTitle, CardLgLink } from '../components/Card.jsx'
+import { Cards, Card, CardTitle, CardLgLink } from '../components/Card.jsx'
 
 export default function index({ projects }){
-    let i = 0;
     return(
         <Layout>
-            { projects.map(item => (
-                <Card key={ i++ }>
-                    <a href={ item.link }>
-                        <CardTitle title= { item.title} />
-                    </a>
-                    <a href={ item.repository }>
-                        <CardLgLink title="Portfolio" />
-                    </a>
-                </Card>
-            )) }
+            <div className="container">
+                { projects.map((item, index) => (
+                    <Card key={ index }>
+                        <a href={ item.link }>
+                            <CardTitle title={ item.title } />
+                        </a>
+                        <a href={ item.repository }>
+                            <CardLgLink title="Portfolio" />
+                        </a>
+                    </Card>
+                )) }
+            </div>
         </Layout>
     )
 }
