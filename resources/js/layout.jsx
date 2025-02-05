a996d35b01186303a275189405c8f42ebb378d46
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 13:59:23 -0300

    *fix styling*

--- resources/js/layout.jsx
+++ resources/js/layout.jsx
@@ -1,7 +1,7 @@
 export function Layout({ children }){
     return(
         <main
-            className="bg-slate-50 dark:bg-slate-900 text-slate-600 dark:text-slate-50 flex flex-col items-center justify-center text-center"
+            className="bg-slate-50 dark:bg-slate-900 text-slate-900 dark:text-slate-300 min-h-screen antialiased"
         >{ children }</main>
     )
 }
