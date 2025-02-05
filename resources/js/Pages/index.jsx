24949b0400a75dbdcecb21daca31ae1c55dd1450
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 16:34:14 -0300

    *feat* wrap Card component in Cards component

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -5,16 +5,18 @@ export default function index({ projects }){
     return(
         <Layout>
             <div className="container">
-                { projects.map((item, index) => (
-                    <Card key={ index }>
-                        <a href={ item.link }>
-                            <CardTitle title={ item.title } />
-                        </a>
-                        <a href={ item.repository }>
-                            <CardLgLink title="Portfolio" />
-                        </a>
-                    </Card>
-                )) }
+                <Cards>
+                    { projects.map((item, index) => (
+                        <Card key={ index }>
+                            <a href={ item.link }>
+                                <CardTitle title={ item.title } />
+                            </a>
+                            <a href={ item.repository }>
+                                <CardLgLink title="Portfolio" />
+                            </a>
+                        </Card>
+                    )) }
+                </Cards>
             </div>
         </Layout>
     )
