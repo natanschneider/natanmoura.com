36571c710ec3e1dee3a11fc6eb1335a3f7493033
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 16:27:18 -0300

    *feat* create migrations for tables projects | technologies | stack

---
+++ database/migrations/2025_02_05_190411_create_projects_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id('id')->primary();
+            $table->string('name', 255);
+            $table->string('img_url', 255)->nullable();
+            $table->string('url', 255)->nullable();
+            $table->string('repo', 255);
+        });
+
+        Schema::create('technologies', function (Blueprint $table) {
+            $table->id('id')->primary();
+            $table->string('name', 255);
+            $table->string('url', 255);
+        });
+
+        Schema::create('stack', function(Blueprint $table){
+            $table->id('id')->primary();
+            $table->bigInteger('id_project')->references('id')->on('projects');
+            $table->foreign('id_technology')->references('id')->on('technologies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+        Schema::dropIfExists('stack');
+        Schema::dropIfExists('technologies');
+    }
+};
