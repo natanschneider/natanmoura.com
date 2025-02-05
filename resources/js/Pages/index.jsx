80186665d91bc45e323cf3b7fcb2815dc7083444
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 16:36:01 -0300

    *feat* verify if projects object isn't empty

--- resources/js/Pages/index.jsx
+++ resources/js/Pages/index.jsx
@@ -5,18 +5,20 @@ export default function index({ projects }){
     return(
         <Layout>
             <div className="container">
-                <Cards>
-                    { projects.map((item, index) => (
-                        <Card key={ index }>
-                            <a href={ item.link }>
-                                <CardTitle title={ item.title } />
-                            </a>
-                            <a href={ item.repository }>
-                                <CardLgLink title="Portfolio" />
-                            </a>
-                        </Card>
-                    )) }
-                </Cards>
+                { projects.length > 0 && (
+                    <Cards>
+                        { projects.map((item, index) => (
+                            <Card key={ index }>
+                                <a href={ item.link }>
+                                    <CardTitle title={ item.title } />
+                                </a>
+                                <a href={ item.repository }>
+                                    <CardLgLink title="Portfolio" />
+                                </a>
+                            </Card>
+                        )) }
+                    </Cards>
+                )}
             </div>
         </Layout>
     )
