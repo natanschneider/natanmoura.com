05849269c2a9edaca92747ccc74ed6ee06ecb8be
Natan Moura Schneider <natan.schneider@deltasul.com.br>
2025-02-05 16:32:27 -0300

    *feat* create Cards component to wrap group of "Card" component

--- resources/js/components/Card.jsx
+++ resources/js/components/Card.jsx
@@ -1,3 +1,11 @@
+export function Cards({ children }){
+    return (
+        <div className="flex flex-col md:flex-row items-center justify-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">{ children }</div>
+        </div>
+    )
+}
+
 export function Card({ children }){
     return(
         <div className="border-2 border-slate-600 dark:border-slate-50 rounded-md overflow-hidden *:w-full">{ children }</div>
